746c7f12bcc1d6ff7b519acbe60342be6353f5d3
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-07 20:28:40 -0500

    made web server use specificied port

--- web/index.js
+++ web/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import * as fs from 'fs'
+import * as path from 'path'
+const config = JSON.parse(fs.readFileSync("./config.json","utf-8"))
+const { token, clientId } = config
+
 export async function main(){
     const app = express()
-    const port = 3000
+    const port = config.port
     
     app.get('/', (req, res) => {
       res.send('Hello World!')
